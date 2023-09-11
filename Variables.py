e90a6dec9f08ea84cd8facbf143999aec83f0b7a
Suman9496 <shumank135@gmail.com>
2023-09-11 22:02:28 +0530

    initial commit

---
+++ Variables.py
@@ -0,0 +1,35 @@
+"""Variables Practice #1
+Declare two variables, called name and age.
+
+Set the name variable value to "Tony Soprano" and the age value to 51."""
+
+name = "Tony Soprano"
+age = 51
+
+'''
+Variables Practice #2
+Create three variables:
+
+first_name
+
+last_name
+
+full_name
+
+Assign the value "Julia" to first_name, and for last_name, assign the value "Roberts". 
+Finally, build the variable full_name by concatenating these two variables (remember to add a space in between).'''
+
+first_name = "Julia"
+last_name = "Roberts"
+full_name = (first_name + " " + last_name)
+
+'''Variables Practice #3
+Declare the variable course, assign it the value "Python", and print the following sentence:
+
+You are taking a course course
+
+To do this, you must concatenate the first and last parts of the sentence with the variable.
+ Remember to add spaces before and after concatenating the variable to the rest of the text.'''
+
+course = "Python"
+print("You are taking a " + course + " course")
