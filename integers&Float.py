ea62d2015e33257d70915684c864be04f1c482f6
Shuman kumari <95907851+Suman9496@users.noreply.github.com>
2023-09-14 21:59:44 +0530

    initial commit

---
+++ integers&Float.py
@@ -0,0 +1,33 @@
+"""Integers Practice
+Declare a numeric variable named int_num that contains a value of integer type of your choice.
+
+Print the data type of that variable."""
+
+int_num = 30
+print(type(int_num))
+
+'''Floats Practice
+Declare a numeric variable named decimal_num that contains a value of float type of your choice.
+
+Print the data type of that variable.'''
+
+decimal_num = 9.34
+print(type(decimal_num))
+
+'''Data Types Practice
+What type is the result of the sum of 7.5 + 2.5? Write the code to verify it.
+
+To do this, create two variables:
+
+num1 = 7.5
+
+num2 = 2.5
+
+Next, print on the screen the data type that results from the sum of both numbers.
+
+Perform the same exercise in PyCharm to see the result. Can you explain what happens?
+'''
+num1 = 7.5
+num2 = 2.5
+num = (num1 + num2)
+print(type(num))
