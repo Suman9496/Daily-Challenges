ff2bd6b08547121ac0565c741b61159b1b4803f2
Shuman kumari <95907851+Suman9496@users.noreply.github.com>
2023-09-16 22:16:12 +0530

    initial commit

---
+++ Mathematical_operators.py
@@ -0,0 +1,18 @@
+"""Mathematical Operators Practice #1
+Print on the screen the floor division of the following two numbers: 874 divided by 27"""
+x = 874
+y = 27
+print(f"{x // y}")
+
+'''Mathematical Operators Practice #2
+Print on the screen the modulus of 456 divided by 33'''
+x = 456
+y = 33
+
+print(f"{x % y}")
+
+'''Mathematical Operators Practice #3
+Calculate and print the square root of 783'''
+x = 783
+
+print(f"{x ** 0.5}")
